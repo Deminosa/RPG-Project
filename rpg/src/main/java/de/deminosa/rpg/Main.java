1bdf3cec45c94350b184a4cb1192daa068fab283
Deminosa <daniel@noennecke.de>
2022-10-13 15:41:01 +0200

    Init Commit

---
+++ rpg/src/main/java/de/deminosa/rpg/Main.java
@@ -0,0 +1,17 @@
+package de.deminosa.rpg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+    
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
